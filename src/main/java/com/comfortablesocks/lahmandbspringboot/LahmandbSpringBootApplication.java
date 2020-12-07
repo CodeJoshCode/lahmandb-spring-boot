948baf3280ebc86b67876206e35f9a7d13541a4c
Josh <simmonsj711@dupage.edu>
2020-12-07 02:39:44 -0600

    first commit

---
+++ src/main/java/com/comfortablesocks/lahmandbspringboot/LahmandbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.comfortablesocks.lahmandbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LahmandbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LahmandbSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comfortablesocks/lahmandbspringboot/LahmandbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comfortablesocks.lahmandbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LahmandbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
